7a7c9f1295616ccee0d2763dc23b7eede2d70481
charleke2611 <charles261105@gmail.com>
2025-11-06 19:34:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[u/@@GyZiCj#D5r$]@db.agcbqqxapmdmdprcvblj.supabase.co:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
